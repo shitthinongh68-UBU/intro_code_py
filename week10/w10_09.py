4ea0d1db47910089353ddab32514fb16d2068d01
shitthinongh68-UBU <shitthinon.gh.68@ubu.ac.th>
2025-09-13 15:34:49 +0700

    fix w10_09.py for gradescope pass all

--- week10/w10_09.py
+++ week10/w10_09.py
@@ -1,12 +1,15 @@
 count = int(input())
 text_list = list()
 
+
 for i in range(count):
+    text_lenght = vowels_count = 0
     text_list.append(input().strip())
     text_for_process = text_list[i].replace(' ','').lower()
-    text_lenght = len(text_for_process)
-    vowels_count = 0
     for e in text_for_process:
+        if e.isalpha():
+            text_lenght += 1
         if e in 'aeiou':
             vowels_count += 1
+    
     print(text_list[i], text_lenght - vowels_count, vowels_count)
