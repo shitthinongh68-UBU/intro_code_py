4e3ec2d6aa8d084fff0b9521c20ed3afa739221a
Shitthinon Goson <shitthinon.gh.68@ubu.ac.th>
2025-09-19 13:59:32 +0700

    csv read in various methods

---
+++ inclass_w11/fileread_csv.py
@@ -0,0 +1,40 @@
+f = open('inclass_w11/src/notebook_price.csv', 'r')
+
+def showvalue_inlist(list):
+    for l in list:
+        print(l)
+
+def Normalread(file):
+    for line in file:
+        print(line.strip().split())
+
+def conditionreadCSV(file):   
+    for line in file:
+        single_line = line.strip().split(',')
+        try:
+            price = float(single_line[-1])
+            data = single_line
+        except:
+            header = single_line
+            continue
+
+        if price >= 20000: print(data)
+        else: continue
+
+
+def SortedCSV(file):
+    comp_list = list()
+    for line in file:
+        single_line = line.strip().split(',')
+        try:
+            price = float(single_line[-1])
+            data = single_line
+        except:
+            header = single_line
+            continue
+        
+        comp_list.append(data)
+
+    comp_list_sorted = sorted(comp_list, key=lambda x: float(x[-1]))
+    showvalue_inlist(comp_list_sorted)
+
